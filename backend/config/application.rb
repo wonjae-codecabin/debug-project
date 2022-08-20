10e6dfa63a4abd94d743d4e6e418740e10ef69c9
Wonjae <wonjae@codecabin.com.au>
2022-08-20 15:29:44 +1000

    [backend] - Data import Step 1

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,7 +1,5 @@
 require_relative "boot"
-
 require "rails/all"
-require 'sprockets/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +19,6 @@ module Backend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
+    config.api_only = true
   end
 end
